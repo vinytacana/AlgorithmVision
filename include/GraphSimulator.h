b356e5561d8f65b88cdb18721b7037be8335aece
Vinny Tacaná <vini.tacana@gmail.com>
2026-04-21 01:49:16 -0400

    refactor: encapsulate graph simulator state

--- include/GraphSimulator.h
+++ include/GraphSimulator.h
@@ -5,6 +5,7 @@
 #include <queue>
 #include <stack>
 #include <set>
+#include <unordered_map>
 #include <glm/glm.hpp>
 #include "Shader.h"
 
@@ -26,23 +27,11 @@ struct Edge {
 
 class GraphSimulator {
 public:
-    std::vector<Node> nodes;
-    std::vector<Edge> edges;
-    unsigned int quadVAO, quadVBO, lineVAO, lineVBO;
-
-    GraphAlgorithm selectedAlgo = BFS;
-    bool isRunning = false;
-    bool isFinished = false;
-    int startNode = 0, endNode = -1;
-    int draggedNode = -1;
-    int connectingFrom = -1;
-
-    std::queue<int> q_bfs;
-    std::stack<int> s_dfs;
-    std::set<std::pair<float, int>> pq_dijkstra;
-    std::set<int> visited;
-
     GraphSimulator();
+    ~GraphSimulator();
+    GraphSimulator(const GraphSimulator&) = delete;
+    GraphSimulator& operator=(const GraphSimulator&) = delete;
+
     void reset();
     void start();
     void step();
@@ -52,10 +41,56 @@ public:
     void removeNode(int id);
     void addEdge(int u, int v, float w);
 
+    void setAlgorithm(GraphAlgorithm algorithm);
+    GraphAlgorithm getAlgorithm() const;
+
+    bool isSimulationRunning() const;
+    bool hasFinished() const;
+    void togglePause();
+
+    int getStartNode() const;
+    int getEndNode() const;
+    void setStartNode(int id);
+    void setEndNode(int id);
+    bool hasNode(int id) const;
+    std::vector<int> getNodeIds() const;
+
+    const std::vector<Node>& getNodes() const;
+    const std::vector<Edge>& getEdges() const;
+    void updateUndirectedEdgeWeight(int u, int v, float weight);
+
 private:
+    std::vector<Node> nodes;
+    std::vector<Edge> edges;
+    std::unordered_map<int, std::size_t> nodeIndexById;
+    unsigned int quadVAO = 0;
+    unsigned int quadVBO = 0;
+    unsigned int lineVAO = 0;
+    unsigned int lineVBO = 0;
+
+    GraphAlgorithm selectedAlgo = BFS;
+    bool isRunning = false;
+    bool isFinished = false;
+    int startNode = 0;
+    int endNode = -1;
+    int draggedNode = -1;
+    int connectingFrom = -1;
+    bool wasLeftPressed = false;
+    bool wasRightPressed = false;
+
+    std::queue<int> q_bfs;
+    std::stack<int> s_dfs;
+    std::set<std::pair<float, int>> pq_dijkstra;
+    std::set<int> visited;
+
     void createDefaultGraph();
     void setupMesh();
     int findNodeAt(glm::vec2 pos);
+    int findNodeIndexById(int id) const;
+    Node* getNodeById(int id);
+    const Node* getNodeById(int id) const;
+    void rebuildNodeIndex();
+    void sanitizeSelection();
     void stepBFS();
     void stepDFS();
     void stepDijkstra();
