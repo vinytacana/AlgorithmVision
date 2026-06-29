c91aa1d12b40f90d78f0ecb2d5478ccfa04c0661
Vinny Tacaná <vini.tacana@gmail.com>
2026-06-28 23:53:18 -0400

    build: include local GLAD loader header

--- include/GLLoader.h
+++ include/GLLoader.h
@@ -1,12 +1,6 @@
 #ifndef GL_LOADER_H
 #define GL_LOADER_H
 
-#if defined(ALGORITHM_VISION_USE_GLAD)
-#include <glad/gl.h>
-#define ALGORITHM_VISION_HAS_GLAD 1
-#else
-#include <GL/glew.h>
-#define ALGORITHM_VISION_HAS_GLAD 0
-#endif
+#include <glad/glad.h>
 
 #endif
